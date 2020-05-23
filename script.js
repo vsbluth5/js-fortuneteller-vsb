9db99a2fb8560abde56f3d5d7495ec84e4e7a812
Glitch (scratched-emerald-patch) <none>
2020-05-23 18:14:32 +0000

    🏵🛰 Checkpoint ./otherlists.js:3856783/19656 ./index.html:3856783/1003 ./script.js:3856783/332

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
 console.log("RUNNING")
+const fortuneTeller = document.querySelector("#teller")
+const fortunr = document.querySelector("#words")
+
+fortuneTeller.addeventListener("mouseover", ()=>{
+  console.log("Mouse Over")
+})
+
+fortuneTeller.addeventListener("click", ()=>{
+  console.log("Tell a fortune!")
+})

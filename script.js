535e401b8dd0d7cc7f509074812ab967e303c8e1
Glitch (scratched-emerald-patch) <none>
2020-05-23 18:47:37 +0000

    💃💋 Checkpoint ./script.js:3856783/1243 ./style.css:3856783/217

--- script.js
+++ script.js
@@ -1,22 +1,22 @@
-console.log("RUNNING")
-const fortuneTeller = document.querySelector("#teller")
-const fortune = document.querySelector("#words")
+console.log("RUNNING");
+const fortuneTeller = document.querySelector("#teller");
+const fortune = document.querySelector("#words");
 
-const randint = (a, b)=>{
-  return Math.floor(Math.random()*(b-a))
-}
+const randint = (a, b) => {
+  return Math.floor(Math.random() * (b - a));
+};
 
-fortuneTeller.addEventListener("mouseenter", (event)=>{
-  console.log("Mouse Over")
+fortuneTeller.addEventListener("mouseenter", event => {
+  console.log("Mouse Over");
+  // event.target.classList.add("glow");
+});
+
+fortuneTeller.addEventListener("mouseover", event => {
+  console.log("Mouse Over");
   event.target.classList.add("glow");
-})
+});
 
-ortuneTeller.addEventListener("mouseleave", (event)=>{
-  console.log("Mouse Over")
-  event.target.classList.remove("glow");
-})
-
-fortuneTeller.addEventListener("click", ()=>{
-  console.log("Tell a fortune!")
-  fortune.innerHTML = `${fortunes[randint(0, fortunes.length)]}`
-})
+fortuneTeller.addEventListener("click", () => {
+  console.log("Tell a fortune!");
+  fortune.innerHTML = `${fortunes[randint(0, fortunes.length)]}`;
+});

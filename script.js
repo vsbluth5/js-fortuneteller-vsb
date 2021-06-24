f93286294f1b6a2be8e1481b56547ba26664e087
Glitch (scratched-emerald-patch) <none>
2021-06-24 13:58:05 +0000

    🍏💃 Checkpoint ./script.js:3856783/1645

--- script.js
+++ script.js
@@ -1,22 +1,35 @@
 console.log("RUNNING");
+// get reference to the fortune teller image
 const fortuneTeller = document.querySelector("#teller");
-const fortune = document.querySelector("#words");
+const fortune = document.querySelector("#word");
+console.log(fortune)
 
+// get a reference to the div which displays the words
+
+// define a function to get random integers between a and b
 const randint = (a, b) => {
-  return Math.floor(Math.random() * (b - a));
+  return Math.floor(Math.random() * Math.abs(b - a));
 };
 
 fortuneTeller.addEventListener("mouseenter", event => {
-  console.log("Mouse Enter");
+  // console.log("Mouse Enter");
   fortuneTeller.classList.add("active");
 });
 
 fortuneTeller.addEventListener("mouseout", event => {
-  console.log("Mouse Exit");
+  // console.log("Mouse Exit");
   fortuneTeller.classList.remove("active");
 });
 
+
 fortuneTeller.addEventListener("click", () => {
   console.log("Tell a fortune!");
-  fortune.innerHTML = `${fortunes[randint(0, fortunes.length)]}`;
+  
+  // Have the teller say how many times it has been clicked
+  // Use the innerHTML property
+  
+  // Do we want to add images, change images dependent on the count?
+  
+  // More advanced = get a random entry from the list and display
+  
 });
